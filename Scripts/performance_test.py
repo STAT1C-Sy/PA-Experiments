5d6d4ca2a829bc5aa0f0f2bbae8c95e475aad255
Fynn Weyrich <33841685+STAT1C-Sy@users.noreply.github.com>
2022-01-17 11:26:24 +0100

    Update performance_test.py

--- Scripts/performance_test.py
+++ Scripts/performance_test.py
@@ -10,7 +10,7 @@ POSTGRES  = True
 if __name__ == "__main__":
     l, m, h, vh, vvh = load_dataset_from_folder("14-01-2022_14-18-08-215806")
 
-    data_length = len(m)
+    data_length = len(l)
     step_size = int(data_length/20)
     steps = [i*step_size for i in range(1, 21)]
 
