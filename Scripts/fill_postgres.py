9ba92fa1634b36c8561319f9c8f32b1609b9f4cb
Fynn Weyrich <33841685+STAT1C-Sy@users.noreply.github.com>
2022-02-03 18:32:30 +0100

    Update fill_postgres.py

--- Scripts/fill_postgres.py
+++ Scripts/fill_postgres.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
    dataset_meta = []
    dataset_vo = []
 
-   for d in m:
+   for d in h:
       vo_final = []
       for v in d["customFieldValues"]:
          v["metadata"] = d["uuid"]
